a8a3fc8910db453e430d688007f226481d08be87
idk-aarza <aarzak2012@gmail.com>
2024-10-13 10:50:01 +0530

    avisha

---
+++ avisha.py
@@ -0,0 +1,30 @@
+name= "Anhad"
+age= 10
+school= "GDGPS"
+weight= 29.5
+he_is_a_boy= True
+
+print("name",name)
+print("data type of name is", type(name))
+print('age',age)
+print("data type of age is", type(age))
+print('school',school)
+print('data type of school is', type(school))
+print('weight', weight)
+print("data type of weight is", type(weight))
+print("he_is_a_boy", he_is_a_boy)
+print("data type of he_is_a_boy", type(he_is_a_boy))
+
+age=str(age)
+print(age)
+print('data type of age',type(age))
+weight=int(weight)
+print(weight)
+print('data type of weight', type(weight))
+print('enter your name')
+Name=input('enter your name')
+apple="red"
+mango="orange"
+print('fruits=', apple + mango)
+print(apple.upper())
+print(mango.lower())
